a105017455e70309b755f5208909c69f49d47a3c
Elyn <elyndebrittob@gmail.com>
2024-10-16 17:24:06 -0300

    fazendo classes service

--- grupo4/src/main/java/br/org/serratec/grupo4/service/UsuarioService.java
+++ grupo4/src/main/java/br/org/serratec/grupo4/service/UsuarioService.java
@@ -1,5 +1,56 @@
 package br.org.serratec.grupo4.service;
 
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UsuarioService {
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public Optional<Usuario> buscarPorId(Long id) {
+        Optional<Usuario> usuario = usuarioRepository.findById(id);
+        return usuario;
+    }
+
+    public List<UsuarioDTO> buscarTodos() {
+		List<Usuario> usuarios = usuarioRepository.findAll();
+		
+		List<UsuarioDTO> usuariosDTO = usuarios.stream().map(UsuarioDTO::new).toList();
+		return usuariosDTO;
+	}
+
+    public UsuarioDTO inserir(UsuarioInserirDTO usuarioInserirDTO) throws EmailException, SenhaException {
+		if (!usuarioInserirDTO.getSenha().equals(usuarioInserirDTO.getConfirmarSenha())) {
+			throw new SenhaException("Senha e Confirma Senha não são iguais.");
+		}
+		if (usuarioRepository.findByEmail(usuarioInserirDTO.getEmail()) != null) {
+			throw new EmailException("Email já existente, por favor, insira outro.");
+		}
+		
+		Usuario usuario = new Usuario();
+		usuario.setNome(usuarioInserirDTO.getNome());
+		usuario.setEmail(usuarioInserirDTO.getEmail());
+		usuario.setSenha(usuarioInserirDTO.getSenha());
+		
+		Set<UsuarioPerfil> perfis = new HashSet<>();
+		for (Perfil perfil: usuarioInserirDTO.getPerfis()) {
+			perfil = perfilService.buscar(perfil.getId());
+			UsuarioPerfil usuarioPerfil = new UsuarioPerfil(usuario, perfil, LocalDate.now());
+			perfis.add(usuarioPerfil);
+		}
+		usuario.setUsuarioPerfis(perfis);
+		
+		usuario = usuarioRepository.save(usuario);
+		
+		UsuarioDTO usuarioDTO = new UsuarioDTO(usuario);
+		return usuarioDTO;
+	}
 }
