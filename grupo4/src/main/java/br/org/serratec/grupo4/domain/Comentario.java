78ad26213e744f675ebb9b2cf4c805554f2017a6
RhayPradoF <rhayssa.c.prado@gmail.com>
2024-10-16 17:16:24 -0300

    Adição de Repository simples  correção de classes

--- grupo4/src/main/java/br/org/serratec/grupo4/domain/Comentario.java
+++ grupo4/src/main/java/br/org/serratec/grupo4/domain/Comentario.java
@@ -12,7 +12,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -20,7 +19,6 @@ import lombok.EqualsAndHashCode;
 
 @Data
 @Entity
-@Table
 @EqualsAndHashCode
 public class Comentario {
 	
