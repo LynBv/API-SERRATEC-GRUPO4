55b9d7294ecc873752cc1cc242187a86c2499a0c
Rodrigo5431 <rodrigokarvalho12890@gmail.com>
2024-10-17 14:51:32 -0300

    controller about to be done

--- grupo4/src/main/java/br/org/serratec/grupo4/controller/UsuarioController.java
+++ grupo4/src/main/java/br/org/serratec/grupo4/controller/UsuarioController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -31,57 +30,52 @@ public class UsuarioController {
 
 	@Autowired
 	private UsuarioRepository usuarioRepository;
-	
+
 	@GetMapping
-	public ResponseEntity<List<Usuario>> listar(){
+	public ResponseEntity<List<Usuario>> listar() {
 		return ResponseEntity.ok(usuarioRepository.findAll());
 	}
-	
+
 	@GetMapping("/pagina")
-	public ResponseEntity<Page<Usuario>> listarPaginado(@PageableDefault
-			(direction= Sort.Direction.ASC, page= 0 ,size =8) Pageable pageable){
+	public ResponseEntity<Page<Usuario>> listarPaginado(
+			@PageableDefault(direction = Sort.Direction.ASC, page = 0, size = 8) Pageable pageable) {
 		return ResponseEntity.ok(usuarioRepository.findAll(pageable));
 	}
-	
+
 	@GetMapping("/{id}")
-	public ResponseEntity<Usuario>buscar(@PathVariable Long id){
-		Optional<Usuario>usuarioOpt = usuarioRepository.findById(id);
+	public ResponseEntity<Usuario> buscar(@PathVariable Long id) {
+		Optional<Usuario> usuarioOpt = usuarioRepository.findById(id);
 		if (usuarioOpt.isPresent()) {
 			return ResponseEntity.ok(usuarioOpt.get());
-		}
-		else {
+		} else {
 			return ResponseEntity.notFound().build();
 		}
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<UsuarioDTO>inserir(@Valid @RequestBody UsuarioDTO usuarioDTO){
-		UsuarioDTO usuarioDTO = usuarioRepository.save(usuarioDTO);
-		URI uri = ServletUriComponentsBuilder
-				.fromCurrentRequest()
-				.path("/{id}")
-				.buildAndExpand(usuarioDTO.getId())
+	public ResponseEntity<Usuario> inserir(@Valid @RequestBody Usuario usuario) {
+		Usuario usuarioDTO = usuarioRepository.save(usuario);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(usuarioDTO.getId())
 				.toUri();
 		return ResponseEntity.created(uri).body(usuarioDTO);
 	}
-	
+
 	@PutMapping("/{id}")
-	public ResponseEntity<Usuario> atualizar(@PathVariable Long id, @Valid @RequestBody Usuario usuario){
-       if (usuarioRepository.existsById(id)) {
-    	   usuario.setId(id);
-    	   return ResponseEntity.ok(usuarioRepository.save(usuario));
-       }else {
-    	   return ResponseEntity.notFound().build();
-       }
-    }
-	
+	public ResponseEntity<Usuario> atualizar(@PathVariable Long id, @Valid @RequestBody Usuario usuario) {
+		if (usuarioRepository.existsById(id)) {
+			usuario.setId(id);
+			return ResponseEntity.ok(usuarioRepository.save(usuario));
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
 	@DeleteMapping("/{id}")
-	public ResponseEntity<Usuario> deletar(@PathVariable Long id){
-		if (usuarioRepository.existsById(id)){
+	public ResponseEntity<Usuario> deletar(@PathVariable Long id) {
+		if (usuarioRepository.existsById(id)) {
 			usuarioRepository.deleteById(id);
 			return ResponseEntity.noContent().build();
-		}
-		else {
+		} else {
 			return ResponseEntity.notFound().build();
 		}
 	}
