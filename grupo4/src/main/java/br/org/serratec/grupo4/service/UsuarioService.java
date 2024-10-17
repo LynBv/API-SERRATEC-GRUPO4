601724c7576b1c1a11ea38808f948c7a39f18c86
Elyn <elyndebrittob@gmail.com>
2024-10-17 10:47:27 -0300

    concertando problemas no pomxml

--- grupo4/src/main/java/br/org/serratec/grupo4/service/UsuarioService.java
+++ grupo4/src/main/java/br/org/serratec/grupo4/service/UsuarioService.java
@@ -1,13 +1,12 @@
 package br.org.serratec.grupo4.service;
 
-import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.org.serratec.grupo4.domain.Usuario;
+import br.org.serratec.grupo4.repository.UsuarioRepository;
 
 @Service
 public class UsuarioService {
@@ -19,38 +18,4 @@ public class UsuarioService {
         Optional<Usuario> usuario = usuarioRepository.findById(id);
         return usuario;
     }
-
-    public List<UsuarioDTO> buscarTodos() {
-		List<Usuario> usuarios = usuarioRepository.findAll();
-		
-		List<UsuarioDTO> usuariosDTO = usuarios.stream().map(UsuarioDTO::new).toList();
-		return usuariosDTO;
-	}
-
-    public UsuarioDTO inserir(UsuarioInserirDTO usuarioInserirDTO) throws EmailException, SenhaException {
-		if (!usuarioInserirDTO.getSenha().equals(usuarioInserirDTO.getConfirmarSenha())) {
-			throw new SenhaException("Senha e Confirma Senha não são iguais.");
-		}
-		if (usuarioRepository.findByEmail(usuarioInserirDTO.getEmail()) != null) {
-			throw new EmailException("Email já existente, por favor, insira outro.");
-		}
-		
-		Usuario usuario = new Usuario();
-		usuario.setNome(usuarioInserirDTO.getNome());
-		usuario.setEmail(usuarioInserirDTO.getEmail());
-		usuario.setSenha(usuarioInserirDTO.getSenha());
-		
-		Set<UsuarioPerfil> perfis = new HashSet<>();
-		for (Perfil perfil: usuarioInserirDTO.getPerfis()) {
-			perfil = perfilService.buscar(perfil.getId());
-			UsuarioPerfil usuarioPerfil = new UsuarioPerfil(usuario, perfil, LocalDate.now());
-			perfis.add(usuarioPerfil);
-		}
-		usuario.setUsuarioPerfis(perfis);
-		
-		usuario = usuarioRepository.save(usuario);
-		
-		UsuarioDTO usuarioDTO = new UsuarioDTO(usuario);
-		return usuarioDTO;
-	}
 }
