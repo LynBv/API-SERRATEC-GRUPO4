93950bc1938db206aeb5fef1d187de6ae11eb435
pckzin01 <patrickgoncalvesdepaiva@gmail.com>
2024-10-17 14:58:01 -0300

    Subindo atualizações do package dto

--- grupo4/src/main/java/br/org/serratec/grupo4/domain/Usuario.java
+++ grupo4/src/main/java/br/org/serratec/grupo4/domain/Usuario.java
@@ -1,10 +1,16 @@
 package br.org.serratec.grupo4.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
@@ -21,23 +27,42 @@ import lombok.EqualsAndHashCode;
 @Data
 @Entity
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class Usuario {
+public class Usuario implements UserDetails, Serializable {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@EqualsAndHashCode.Include
-	private Long id;
-	private String nome;
-	private String sobrenome;
-	private String email;
-	private String senha;
-	private LocalDate dataNascimento;
+    private static final long serialVersionUID = 1L;
 
-	@JsonManagedReference
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+    private String nome;
+    private String sobrenome;
+    private String email;
+    private String senha;
+    private LocalDate dataNascimento;
+
+    @JsonManagedReference
     @OneToMany(mappedBy = "usuario", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<Postagem> postagens = new ArrayList<>();
 
-	@OneToMany(mappedBy = "id.usuario", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-	private Set<UsuarioRelacionamento> usuarioPerfis = new HashSet<>();
+    @OneToMany(mappedBy = "id.usuario", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Set<UsuarioRelacionamento> usuarioPerfis = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+    
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
 
 }
