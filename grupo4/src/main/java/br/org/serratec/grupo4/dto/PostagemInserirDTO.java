2c570ef2650d011f88ed7102645edc2884a7259b
Elyn <elyndebrittob@gmail.com>
2024-10-18 13:27:39 -0300

    subindo authentication e authorization e classes de servico de postagem e comentario

--- grupo4/src/main/java/br/org/serratec/grupo4/dto/PostagemInserirDTO.java
+++ grupo4/src/main/java/br/org/serratec/grupo4/dto/PostagemInserirDTO.java
@@ -1,16 +1,13 @@
 package br.org.serratec.grupo4.dto;
 
 import java.time.LocalDate;
-import java.util.List;
 
-import br.org.serratec.grupo4.domain.Comentario;
 import br.org.serratec.grupo4.domain.Usuario;
 
 public class PostagemInserirDTO {
 
 	private String conteudo;
 	private LocalDate dataCriacao;
-	private List<Comentario> comentarios;
 	private Usuario usuario;
 
 	public String getConteudo() {
@@ -27,14 +24,6 @@ public class PostagemInserirDTO {
 
 	public void setDataCriacao(LocalDate dataCriacao) {
 		this.dataCriacao = dataCriacao;
-	}
-
-	public List<Comentario> getComentarios() {
-		return comentarios;
-	}
-
-	public void setComentarios(List<Comentario> comentarios) {
-		this.comentarios = comentarios;
 	}
 
 	public Usuario getUsuario() {
