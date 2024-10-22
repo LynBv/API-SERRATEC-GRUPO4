20e8e404c3917c2510ed577f3f08f67e8968dcd7
Rayssa <132487508+rayvieira307@users.noreply.github.com>
2024-10-22 18:51:37 -0300

    Update SwaggerConfig.java

--- grupo4/src/main/java/br/org/serratec/grupo4/config/SwaggerConfig.java
+++ grupo4/src/main/java/br/org/serratec/grupo4/config/SwaggerConfig.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
@@ -13,8 +13,6 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
 
-
-@EnableWebSecurity
 @Configuration
 public class SwaggerConfig {
 	
