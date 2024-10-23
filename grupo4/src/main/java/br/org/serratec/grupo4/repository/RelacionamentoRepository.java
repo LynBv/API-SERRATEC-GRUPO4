373be4c132d4b735427d2424b3ab6e97e846f2f0
RhayPradoF <rhayssa.c.prado@gmail.com>
2024-10-23 14:34:46 -0300

    Atualização final

--- grupo4/src/main/java/br/org/serratec/grupo4/repository/RelacionamentoRepository.java
+++ grupo4/src/main/java/br/org/serratec/grupo4/repository/RelacionamentoRepository.java
@@ -13,25 +13,24 @@ import br.org.serratec.grupo4.dto.RelacionamentoDTO;
 
 @Repository
 public interface RelacionamentoRepository extends JpaRepository<Relacionamento, UsuarioRelacionamentoPK> {
-	
-	  @Query(value = """
-            SELECT SGD.NOME AS nome_seguido, SG.NOME AS nome_seguidor, R.DATA_INICIO_SEGUIMENTO AS data_inicio_seguimento
-            FROM RELACIONAMENTO AS R
-            INNER JOIN USUARIO AS SGD ON R.ID_SEGUIDOR = SGD.ID_USUARIO
-            INNER JOIN USUARIO AS SG ON R.ID_SEGUIDO = SG.ID_USUARIO
-            WHERE R.ID_SEGUIDOR = :usuarioId
-            ORDER BY SG.NOME
-            """, nativeQuery = true)
-    List<RelacionamentoDTO> findSeguidoresPorUsuarioId(@Param("usuarioId") Long usuarioId);
 
-    @Query(value = """
+	@Query(value = """
+			 SELECT SGD.NOME AS nome_seguidor, SG.NOME AS nome_seguido, R.DATA_INICIO_SEGUIMENTO AS data_inicio_seguimento
+			FROM RELACIONAMENTO AS R
+			INNER JOIN USUARIO AS SGD ON R.ID_SEGUIDOR = SGD.ID_USUARIO
+			INNER JOIN USUARIO AS SG ON R.ID_SEGUIDO = SG.ID_USUARIO
+			WHERE R.ID_SEGUIDOR = :usuarioId
+			ORDER BY SG.NOME;
+			""", nativeQuery = true)
+	List<RelacionamentoDTO> findSeguidoresPorUsuarioId(@Param("usuarioId") Long usuarioId);
 
-    		SELECT SG.NOME AS nome_seguidor, SGD.NOME AS nome_seguido, R.DATA_INICIO_SEGUIMENTO AS data_inicio_seguimento
-            FROM RELACIONAMENTO AS R
-            INNER JOIN USUARIO AS SG ON R.ID_SEGUIDOR = SG.ID_USUARIO
-            INNER JOIN USUARIO AS SGD ON R.ID_SEGUIDO = SGD.ID_USUARIO
-            WHERE R.ID_SEGUIDO = :usuarioId
-            ORDER BY SGD.NOME
+	@Query(value = """
+			SELECT SGD.NOME AS nome_seguido, SG.NOME AS nome_seguidor, R.DATA_INICIO_SEGUIMENTO AS data_inicio_seguimento
+			FROM RELACIONAMENTO AS R
+			INNER JOIN USUARIO AS SGD ON R.ID_SEGUIDOR = SGD.ID_USUARIO
+			INNER JOIN USUARIO AS SG ON R.ID_SEGUIDO = SG.ID_USUARIO
+			WHERE R.ID_SEGUIDO = :usuarioId
+			ORDER BY SG.NOME
             """, nativeQuery = true)
-    List<RelacionamentoDTO> findSeguindoPorUsuarioId(@Param("usuarioId") Long usuarioId);
+	List<RelacionamentoDTO> findSeguindoPorUsuarioId(@Param("usuarioId") Long usuarioId);
 }
