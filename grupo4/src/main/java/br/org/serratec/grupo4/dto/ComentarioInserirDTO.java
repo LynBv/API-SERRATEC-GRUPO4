8d7fa50b949a24d5fa3b23111b82bfef7fd95cd0
Rodrigo5431 <rodrigokarvalho12890@gmail.com>
2024-10-22 22:19:49 -0300

    controller relacionamento

--- grupo4/src/main/java/br/org/serratec/grupo4/dto/ComentarioInserirDTO.java
+++ grupo4/src/main/java/br/org/serratec/grupo4/dto/ComentarioInserirDTO.java
@@ -10,12 +10,9 @@ public class ComentarioInserirDTO {
 	@NotBlank(message = "Comentário não pode estar em branco!!")
 
 	@Size(max = 400, message = "Comentario não pode ultraprassar o limite de (max) caracteres!!")
-	@Schema(description = "Id do Texto")
+	@Schema(description = "Conteúdo do comentario")
 	private String texto;
-	
-	@Schema(description = "Id da Postagem")
-	@Size(max = 400, message = "Comentário não pode ultrapassar o limite de (max) caracteres!!")
-	private String texto;
+
 	
 	@NotNull(message = "ID da Postagem não pode estar vazio!!")
 	private Long idPostagem;
